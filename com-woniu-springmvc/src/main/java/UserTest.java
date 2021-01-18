4b6210fe2ea893a74b408743399bb7bf79a754e8
xiaokongming <1827818850@qq.com>
2021-01-18 19:53:52 +0800

    springmvc

--- com-woniu-springmvc/src/main/java/UserTest.java
+++ com-woniu-springmvc/src/main/java/UserTest.java
@@ -3,4 +3,9 @@ public class UserTest {
     public static void main(String[] args) {
         System.out.println("aaaaaaaaaaaaaaaaaaaaaa");
     }
+
+
+    public void show(){
+        System.out.println("sdssddsdsdsdsdds");
+    }
 }
